2daeed42abe75073b0565da02a8c65847bbb2215
unknown <davidbassouto@hotmail.com>
2023-02-02 09:28:05 -0300

    feat: data validation on products CRUD

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,56 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/ProductController';
 
+import { celebrate, Joi, Segments } from 'celebrate';
+
 const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.index);
-productsRouter.get('/:id', productsController.listProductById);
-productsRouter.post('/', productsController.createProduct);
-productsRouter.patch('/:id', productsController.updateProduct);
-productsRouter.delete('/:id', productsController.deleteProduct);
+
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.listProductById,
+);
+
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.createProduct,
+);
+productsRouter.patch(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string(),
+      price: Joi.number().precision(2),
+      quantity: Joi.number(),
+    },
+  }),
+  productsController.updateProduct,
+);
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.deleteProduct,
+);
 
 export default productsRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import 'express-async-errors';
 import { routes } from './routes';
 import { AppError } from '../errors/AppError';
+import { errors } from 'celebrate';
 import '../typeorm';
 
 const app = express();
@@ -11,22 +13,25 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
+app.use(
+  (error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    res.status(500).json({
       status: 'error',
-      message: error.message,
+      message: 'Internal server error',
     });
-  }
 
-  res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-
-  next();
-});
+    next();
+  },
+);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('Server starded on port 3333!✨');
